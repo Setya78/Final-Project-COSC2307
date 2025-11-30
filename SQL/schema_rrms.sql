c4cbbcbe169f7c8bcd416dcf698c8cfd112d040b
deny setiawan rahardjo <denysetiawan03@gmail.com>
2025-11-29 20:29:08 -0500

    Add landlord, property, and unit tables to schema

--- SQL/schema_rrms.sql
+++ SQL/schema_rrms.sql
@@ -1,1 +1,32 @@
 
+
+--=========================================
+--1. MASTER DATA
+--=========================================
+
+--1.1 LANDLORD
+
+CREATE TABLE landlord (
+	landlord_id		SERIAL PRIMARY KEY,
+	first_name 		VARCHAR(100) NOT NULL,
+	last_name		VARCHAR(100) NOT NULL,
+	business_name	VARCHAR(150),
+	email			VARCHAR(150)UNIQUE NOT NULL,
+	phone			VARCHAR(20),
+	interac_contact	VARCHAR(150),
+);
+
+--1.2 PROPERTY
+CREATE TABLE property (
+	property_id		SERIAL PRIMARY KEY,
+	lanlord_id 		INT NOT NULL REFERENCES landlord(lanlord_id),
+	property_name	VARCHAR(150),
+	property_type	VARCHAR(50),
+	full_address	TEXT,
+	post_code		VARCHAR(15),
+);
+
+-- 1.3 UNIT
+CREATE TABLE unit (
+	unit_id 		SERIAL NUMBER KEY,
+	)
